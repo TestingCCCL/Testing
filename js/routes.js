6da3391a26d2427aa4916ac59263ebc4cf273153
FudezPal <76588336+FudezPal@users.noreply.github.com>
2025-12-24 23:03:44 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import ConixChallenges from './pages/Conix Challenges.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/roulette', component: Conix Challenges },
 ];
